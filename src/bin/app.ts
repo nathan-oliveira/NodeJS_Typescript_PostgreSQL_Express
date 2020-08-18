fb21af0ddd72351970f138250c24f99e62dbf9cf
Nathan Oliveira <52627147+nathan-oliveira@users.noreply.github.com>
2020-08-18 20:09:39 -0300

    Update app.ts

--- src/bin/app.ts
+++ src/bin/app.ts
@@ -2,15 +2,13 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import express from 'express';
 import * as bp from 'body-parser';
+import cors from 'cors';
 
 // Routes
 import { Index as IndexRouter } from '@src/routes/index'
 import { Test as TestRouter } from '@src/routes/test/test'
 
-// import * as cors from 'cors';
 // import * as consign from 'consign';
-// consign({extensions: ['.ts']})
-// import {}
 
 class App {
     public app: express.Application
@@ -19,20 +17,33 @@ class App {
 
     constructor() {
         this.app = express();
+        this.cors();
         this.middleware();
         this.routes();
     }
 
     private middleware(): void {
-        // this.app.use(cors());
         this.app.use(bp.urlencoded({ extended: true }))
         this.app.use(bp.json({ limit: '20mb' }))
         this.app.use(express.static('public'))
     }
-    
+
     private routes(): void {
         this.indexRouter.routes(this.app);
         this.testRouter.routes(this.app);
+    }
+
+    private cors(): void {
+        const options:cors.CorsOptions = {
+            allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+            credentials: true,
+            methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+            origin: 'http://localhost:4000',
+            preflightContinue: false
+        };
+
+        this.app.use(cors(options))
+        this.app.options("*", cors(options))
     }
 }
 
